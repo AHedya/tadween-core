99cf209e7e42691dc5b69cc35359d6f12f0028e4
Abdulrahman Hedya <abdohedya19@gmail.com>
2026-04-01 13:30:38 +0200

    test(S3Repo)
    Mark TestS3Repo test class as an xfail becasue of moto not being reilable with free-threaded builds: 3.14t

--- tests/repo/test_s3.py
+++ tests/repo/test_s3.py
@@ -3,7 +3,13 @@ import pytest
 from .test_contract import RepoContract
 
 
+@pytest.mark.xfail(
+    reason="moto.s3.urls *sometimes* fails in free-threaded build", raises=ImportError
+)
 class TestS3Repo(RepoContract):
     @pytest.fixture
     def repo(self, s3_repo):
         return s3_repo
+
+    def test_concurrent_writes(self):
+        pytest.skip("moto is not thread-safe.")
