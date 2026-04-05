fc72e7af07a74a6814b924fecc55edad43bbf011
Abdulrahman Hedya <abdohedya19@gmail.com>
2026-04-05 05:19:49 +0200

    ref (BaseCache)
    Refactor BaseCache contract to use ABC instead of duck-typing protocol + runtime_checkable.

--- src/tadween_core/cache/simple_cache.py
+++ src/tadween_core/cache/simple_cache.py
@@ -1,10 +1,12 @@
 import threading
-from typing import Generic, TypeVar
+from typing import TypeVar
+
+from tadween_core.cache.base import BaseCache
 
 BucketSchemaT = TypeVar("BucketSchemaT")
 
 
-class SimpleCache(Generic[BucketSchemaT]):
+class SimpleCache(BaseCache[BucketSchemaT]):
     """Type-safe, thread-safe alternative to `Cache`.
 
     All public methods are thread-safe as they are just internal dict wrappers. However,
@@ -22,11 +24,15 @@ class SimpleCache(Generic[BucketSchemaT]):
     def __init__(self, schema_type: type[BucketSchemaT]) -> None:
         self._schema_type = schema_type
         self._store: dict[str, BucketSchemaT] = {}
-        self.lock = threading.RLock()
+        self._lock = threading.RLock()
 
     @property
     def schema_type(self) -> type[BucketSchemaT]:
         return self._schema_type
+
+    @property
+    def lock(self):
+        return self._lock
 
     def get_bucket(self, key: str) -> BucketSchemaT | None:
         """Retrieve a cached instance by key. Returns None if not found."""
